51defde9aff8d3f7ecc1719490cbc1da78a46474
Harpreet <ksfibri@gmail.com>
2026-06-02 17:51:27 -0500

    Add link in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './style/index.css';
+import './style/reset.css';
+import './style/index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
 );
 
-
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
