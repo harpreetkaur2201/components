801bda35c87a8ea6cf0f4ad7f2c7c0a48226e796
Harpreet <ksfibri@gmail.com>
2026-06-02 18:16:39 -0500

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Gallery from './components/Gallery';
+import Footer from './components/Footer';
+
 function App() {
-  return (
-   <></>
-  );
+
+    const navLinks = [
+        { index: 0, name: 'Home', url: '#' },
+        { index: 1, name: 'About', url: '#' },
+        { index: 2, name: 'Gallery', url: '#' },
+        { index: 3, name: 'Contact', url: '#' }
+    ];
+
+    let bannerTitle = 'Be creative';
+
+    let bannerText =
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
+    const galleryImages = [
+        'https://picsum.photos/200?1',
+        'https://picsum.photos/200?2',
+        'https://picsum.photos/200?3',
+        'https://picsum.photos/200?4'
+    ];
+
+    return (
+        <>
+            <Header navLinks={navLinks} />
+
+            <Banner
+                title={bannerTitle}
+                text={bannerText}
+            />
+
+            <Gallery images={galleryImages} />
+
+            <Footer />
+        </>
+    );
 }
 
 export default App;
