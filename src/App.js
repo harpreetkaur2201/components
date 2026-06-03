af068df5bf4a506f214b3c5b0fbaacb1b8eef560
Harpreet <ksfibri@gmail.com>
2026-06-03 10:27:20 -0500

    style the images

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Header from './components/Header';
 import Banner from './components/Banner';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
-import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
-import image4 from './images/image4.jpg';
+import image1 from './images/image1.png';
+import image2 from './images/image2.png';
+import image3 from './images/image3.png';
+import image4 from './images/image4.png';
+
 
 function App() {
 
@@ -21,13 +22,12 @@ function App() {
     let bannerText =
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
-  const galleryImages = [
+       const galleryImages = [
     image1,
     image2,
     image3,
     image4
 ];
-
     return (
         <>
             <Header navLinks={navLinks} />
